6c4e3fd318c527005cc2eb3c9873cd1618864fb4
Joey M <172998670+mankovich@users.noreply.github.com>
2024-08-01 12:00:38 -0400

    pushing updates, continuing to try to work through this

--- db/query.sql
+++ db/query.sql
@@ -1,4 +1,4 @@
--- JOIN query combining content of three tables without unnecessary id metadata...BUT leaving manager_id referential values for now because I haven't yet figured out how to populate that column with the names of the employees to whom it refers
+-- JOIN query combining content of three tables, less the unnecessary id metadata...BUT leaving manager_id referential values for now because I haven't yet figured out how to populate that column with the names of the employees to whom it refers
 SELECT employee.first_name AS first, employee.last_name AS last, role.title AS title, role.salary AS salary, department.name as department, employee.manager_id AS manager_id
 FROM employee
 JOIN role ON employee.role_id = role.id
---
+++ index.js
@@ -0,0 +1,4 @@
+const prompts = require('./utils/prompt.js')
+const Emp_Database = require('./utils/queries.js')
+const taskTime = require('./utils/prompt.js')
+

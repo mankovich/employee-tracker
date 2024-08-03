12eab4b85ea79b64ad541816d5923a2bd70de451
Joey M <172998670+mankovich@users.noreply.github.com>
2024-08-03 04:57:08 -0400

    pushing working updates to code...

--- db/query.sql
+++ db/query.sql
@@ -1,5 +1,11 @@
 -- JOIN query combining content of three tables, less the unnecessary id metadata...BUT leaving manager_id referential values for now because I haven't yet figured out how to populate that column with the names of the employees to whom it refers
-SELECT employee.first_name AS first, employee.last_name AS last, role.title AS title, role.salary AS salary, department.name as department, employee.manager_id AS manager_id
+SELECT 
+    employee.first_name, 
+    employee.last_name, 
+    role.title AS title, 
+    role.salary AS salary, 
+    department.name AS department, 
+    employee.manager_id AS manager_id
 FROM employee
 JOIN role ON employee.role_id = role.id
 JOIN department ON role.department_id = department.id;
--- index.js
+++ index.js
@@ -1,5 +1,87 @@
-// const prompts = require('./utils/prompt.js')
-const Emp_Database = require('./lib/queries.js')
-const startTask = require('./lib/prompt.js')
+const { default: inquirer } = require('inquire')
+const { Emp_Database } = require('./lib/query.js') /* ????? */
 
 
+
+
+
+//set variable options as an array strings of each options presented to the user
+const options = [
+    'View all departments',
+    'View all roles',
+    'View all employees',
+    'Add a department',
+    'Add a role',
+    'Add an employee',
+    "Update an employee's role",
+    'Quit'
+];
+
+const start = async () => {
+    await inquirer.prompt([
+        {
+            type: 'list',
+            message: '\nWhat would you like to do?\n',
+            name: 'choice'
+            choices: options,
+        }
+    ])
+    .then((ans) => {
+        if (ans.choice === 'Quit') {
+            console.log('++++++++++\n\nGoodbye\n\n++++++++++');
+            process.end();
+        } else if (ans.choice === 'View all departments') {
+            await printDepts();
+        } else if (ans.choice === 'View all roles') {
+            await  printRoles();
+        } else if (ans.choice === 'View all employees') {
+            await printEmps();
+        } else if (ans.choice === 'Add a department') {
+            await addDept(newDept);
+        } else if (ans.choice === 'Add a role') {
+            await addRole();
+        } else if (ans.choice === 'Add an employee') {
+            await addEmp();
+        } else if (ans.choice === "Update an employee's role") {
+            await updateEmpRole();
+        } 
+    })
+};
+
+const promptNewDept = async () => {
+    const newDept = await inquirer.prompt([
+        {
+            type: 'input',
+            message: 'What is the name of the department that you would like to add?',
+            name: 'name'
+        }
+    ]);
+    return newDept;
+};
+
+const promptNewRole = async () => {
+    const departments = await Emp_Database.getDepts();
+    const newRole = await inquirer.prompt([
+        {
+            type: 'input',
+            message: "What is the title of the new role that you would like to add?'",
+            name: 'title'
+        }
+        {
+            type: 'list',
+            message: 'What department will the new role fall under?',
+            choices: departments,
+            name: 'department'
+        }
+        {
+            type: 'input',
+            message: 'What will be the salary for the new role?'
+            name: 'salary'
+        }
+    ]);
+    return newRole;
+};
+
+const start = 
+
+module.exports = start();
